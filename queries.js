de0e0510dc3b76709e964b1e7306c4f0917d9982
Jettapat <hennet2@hotmail.com>
2019-10-31 19:38:12 +0700

    Update queries.js

--- queries.js
+++ queries.js
@@ -83,7 +83,9 @@ const getProdById3offer = (request, response) => {
 //QRCODE
 const getProdById3 = (request, response) => {
   const id = request.params.id
-  pool.query('SELECT * FROM public_b1.retail_comp WHERE item_id = $1 and timestamp IN (SELECT max(timestamp) FROM public_b1.retail_comp) ORDER BY price ASC limit 1;', [id], (error, results) => {
+  //pool.query('SELECT * FROM public_b1.retail_comp WHERE item_id = $1 and timestamp IN (SELECT max(timestamp) FROM public_b1.retail_comp) ORDER BY price ASC limit 1;', [id], (error, results) => {
+  
+  pool.query('SELECT * FROM public_b1.retail_comp AS RC INNER JOIN (SELECT itemcode,upsale FROM  public_b1.item_margin WHERE day=' + day + 'and month=' + month + '  ) DT ON DT.ItemCode=RC.item_id  WHERE item_id = $1 and timestamp IN (SELECT max(timestamp) FROM public_b1.retail_comp) ORDER BY price ASC limit 1;', [id], (error, results) => {
     //pool.query('SELECT * FROM public_b1.retail_comp WHERE item_id = $1;', [id], (error, results) => {
     if (error) {
       throw error
